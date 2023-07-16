dd2e5a9fb3eee593f1852f80bce731db53936d54
Trevor800 <jeynator780@gmail.com>
2023-07-16 12:55:13 -0700

    my commit

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
+"""Basemodel class """
 import uuid
 from datetime import datetime
 import models
-
-
 class BaseModel:
     def __init__(self, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-
+        for key, value in kwargs.items():
+            setattr(self, key, value)
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
     def save(self):
         self.updated_at = datetime.now()
-
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
