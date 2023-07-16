68dcc041d010ca15ab9847d99d190bf126e5d774
Trevor800 <jeynator780@gmail.com>
2023-07-16 09:04:32 -0700

    test 1

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+
 import models
 from uuid import uuid4
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
-from sqlalchemy import DateTime
-from sqlalchemy import String
+from sqlalchemy import Column, DateTime, String
 
 Base = declarative_base()
 
-
-class BaseModel:
+class BaseModel(Base):
     """Defines the BaseModel class.
     Attributes:
         id (sqlalchemy String): The BaseModel id.
         created_at (sqlalchemy DateTime): The datetime at creation.
         updated_at (sqlalchemy DateTime): The datetime of last update.
     """
+
+    __tablename__ = 'base_model'
 
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
