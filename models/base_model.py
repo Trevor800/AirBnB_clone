f6106b01d0d239fa9581d10b06c214c341a08c63
Trevor800 <jeynator780@gmail.com>
2023-07-16 11:27:36 -0700

    work it out

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-"""Basemodel class """
-import uuid
 from datetime import datetime
-import models
-class BaseModel:
-    def __init__(self, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    def save(self):
-        self.updated_at = datetime.now()
-    def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+import uuid
+
+def create_base_model(**kwargs):
+    base_model = {
+        'id': str(uuid.uuid4()),
+        'created_at': datetime.now(),
+        'updated_at': datetime.now()
+    }
+    base_model.update(kwargs)
+    
+    def __str__():
+        return "[{}] ({}) {}".format(base_model['__class__'], base_model['id'], base_model)
+    
+    def save():
+        base_model['updated_at'] = datetime.now()
+    
+    def to_dict():
+        obj_dict = base_model.copy()
+        obj_dict['__class__'] = base_model['__class__']
+        obj_dict['created_at'] = base_model['created_at'].isoformat()
+        obj_dict['updated_at'] = base_model['updated_at'].isoformat()
         return obj_dict
+    
+    base_model['__str__'] = __str__
+    base_model['save'] = save
+    base_model['to_dict'] = to_dict
+    
+    return base_model
